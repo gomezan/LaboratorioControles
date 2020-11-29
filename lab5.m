6c78d7b7266aaaf1777ff85c40addc7765eca904
AbelardoValdivieso <valdivieso9901@gmail.com>
2020-11-28 23:43:49 -0500

    Abelardo

---
+++ lab5.m
@@ -0,0 +1,46 @@
+Datos= xlsread('Datos.xlsx')
+x=Datos(:,1);
+y=Datos(:,2);
+
+%%modelo primer orden + tiempo muerto %%
+s=tf('s');
+To= 1.3515;
+num=(exp(-To*s)*0.6473);
+den=(5.6745*s+1)
+f=num/den;
+
+NUM=[0.6473];
+DEN=[5.6745 1];
+
+%PID
+% P=2.6302;
+% I=0.54967;
+% D=1.06;
+% N=0.53998;
+% % modelo=tf(num,den);
+
+figure()
+hold on
+plot(x,y,'red');
+step(f);
+hold off
+
+% Calculo del error
+
+% for t1=0:29.7:0.53
+% [y1,t1] = step(f,'r');
+% 32.9263681576492
+% end
+% [y1,t1] = step(f,'r');
+% Y1=y1(1:56);
+% T1=t1(1:56);
+% figure()
+% hold on
+% plot(t1,y1); title 'y1'
+% plot(x,y,'red');
+% hold off
+% 
+% ERROR = ((Y1 -y)./y)*100;
+% figure()
+% plot(x,ERROR); title 'Error'
+% % err = immse(Y1,y);
