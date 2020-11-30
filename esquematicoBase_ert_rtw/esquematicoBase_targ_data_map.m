2175747d4e40022417e3a05faae3bd1d38b78c8c
gomezan <36377557+gomezan@users.noreply.github.com>
2020-11-30 02:21:51 -0500

    primera prueba pwm

---
+++ esquematicoBase_ert_rtw/esquematicoBase_targ_data_map.m
@@ -0,0 +1,240 @@
+  function targMap = targDataMap(),
+
+  ;%***********************
+  ;% Create Parameter Map *
+  ;%***********************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 1;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc paramMap
+    ;%
+    paramMap.nSections           = nTotSects;
+    paramMap.sectIdxOffset       = sectIdxOffset;
+      paramMap.sections(nTotSects) = dumSection; %prealloc
+    paramMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (gamgcsukny)
+    ;%
+      section.nData     = 2;
+      section.data(2)  = dumData; %prealloc
+      
+	  ;% gamgcsukny.AnalogInput_SampleTime
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% gamgcsukny.Constant_Value
+	  section.data(2).logicalSrcIdx = 1;
+	  section.data(2).dtTransOffset = 1;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(1) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (parameter)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    paramMap.nTotData = nTotData;
+    
+
+
+  ;%**************************
+  ;% Create Block Output Map *
+  ;%**************************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 1;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc sigMap
+    ;%
+    sigMap.nSections           = nTotSects;
+    sigMap.sectIdxOffset       = sectIdxOffset;
+      sigMap.sections(nTotSects) = dumSection; %prealloc
+    sigMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (pcevguwwg1g)
+    ;%
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% pcevguwwg1g.bmlb414dxn
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(1) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (signal)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    sigMap.nTotData = nTotData;
+    
+
+
+  ;%*******************
+  ;% Create DWork Map *
+  ;%*******************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 5;
+    sectIdxOffset = 1;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc dworkMap
+    ;%
+    dworkMap.nSections           = nTotSects;
+    dworkMap.sectIdxOffset       = sectIdxOffset;
+      dworkMap.sections(nTotSects) = dumSection; %prealloc
+    dworkMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (haqxrxtiiae)
+    ;%
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% haqxrxtiiae.ckcme2i2ys
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% haqxrxtiiae.olwofu5ua2
+	  section.data(1).logicalSrcIdx = 1;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(2) = section;
+      clear section
+      
+      section.nData     = 4;
+      section.data(4)  = dumData; %prealloc
+      
+	  ;% haqxrxtiiae.mdvhm2iyde
+	  section.data(1).logicalSrcIdx = 2;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% haqxrxtiiae.a42ofewwte
+	  section.data(2).logicalSrcIdx = 3;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% haqxrxtiiae.ff3echamzb
+	  section.data(3).logicalSrcIdx = 4;
+	  section.data(3).dtTransOffset = 2;
+	
+	  ;% haqxrxtiiae.bhudni4gk3
+	  section.data(4).logicalSrcIdx = 5;
+	  section.data(4).dtTransOffset = 3;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(3) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% haqxrxtiiae.fybf0ymk2l.LoggedData
+	  section.data(1).logicalSrcIdx = 6;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(4) = section;
+      clear section
+      
+      section.nData     = 2;
+      section.data(2)  = dumData; %prealloc
+      
+	  ;% haqxrxtiiae.g3mhl0i21g
+	  section.data(1).logicalSrcIdx = 7;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% haqxrxtiiae.fjmeyym4qs
+	  section.data(2).logicalSrcIdx = 8;
+	  section.data(2).dtTransOffset = 1;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(5) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (dwork)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    dworkMap.nTotData = nTotData;
+    
+
+
+  ;%
+  ;% Add individual maps to base struct.
+  ;%
+
+  targMap.paramMap  = paramMap;    
+  targMap.signalMap = sigMap;
+  targMap.dworkMap  = dworkMap;
+  
+  ;%
+  ;% Add checksums to base struct.
+  ;%
+
+
+  targMap.checksum0 = 2181052576;
+  targMap.checksum1 = 1977287346;
+  targMap.checksum2 = 2555386814;
+  targMap.checksum3 = 2091530516;
+
